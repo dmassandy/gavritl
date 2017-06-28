4b60e4703916c751a23d8aca48e3faf659d82455
Danang Massandy <danang.massandy@icube-mobile.com>
2017-06-28 11:01:05 +0700

    add linz server to allowed_hosts

--- gavritl/settings.py
+++ gavritl/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'j(r4i-3nuraqp-a+25musc%woe6vb$v3=$*qz(#7)16dr0)cy1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['188.166.243.70']
 
 
 # Application definition
